8c7953c60ecfc131dbe087fdc4f8dcae3106f87a
chattertonleon@gmail.com <chattertonleon@gmail.com>
2020-02-06 10:02:13 +0000

    update 2

--- Unit 1/node_intro/hello.js
+++ Unit 1/node_intro/hello.js
@@ -2,3 +2,25 @@ const chai = require('chai');
 const expect = chai.expect;
 const request = require('superagent');
 const status = require('http-status');
+
+const apiRoot = 'http://localhost:3000/';
+
+describe('hello API', function(){
+  it ('GET request text "Hello, World!".',function(done){
+    request.get(apiRoot)
+      .end(function(err,res){
+        expect(err).to.not.be.an('error');
+        expect(res.statusCode).to.equal(status.OK);
+        expect(res.text).to.equal('Hello, World');
+        done();
+      });
+  });
+  it('POST request is not allowed', function(done){
+    request.post(apiRoot)
+      .end(function(err,res){
+        except(err).to.be.an('error');
+        except(res.statusCode).to.equal(status.METHOD_NOT_ALLOWED);
+        done();
+      });
+  });
+});
