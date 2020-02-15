3cd987ad31312d8aa418b24c642a2eceb7730ffe
chattertonleon@gmail.com <chattertonleon@gmail.com>
2020-02-15 17:43:34 +0000

    node_intro/

--- Unit_1/node_intro/lib/messages.js
+++ Unit_1/node_intro/lib/messages.js
@@ -6,12 +6,19 @@ module.exports = function(url,callback){
 
   const Message = mongoose.model(
     'messages',
-    {}
+    {
+      username: String,
+      text: String
+    }
   );
 
   return {
     create:function(newMessage,callback){
-      callback();
+      const message = new Message(
+        {username: newMessage.username, text: newMessage.text});
+      var res = message;
+      var err = null;
+      callback(err,res);
     },
     read:function(id,callback){
       callback();
